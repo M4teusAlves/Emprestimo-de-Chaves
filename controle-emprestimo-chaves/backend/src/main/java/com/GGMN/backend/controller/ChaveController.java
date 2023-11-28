ae48cda78e4ea7195de0332dfb2c74e44091c3b4
M4teusAlves <mateus.alves.desenvolvedor@gmail.com>
2023-11-28 12:11:32 -0300

    Upadte e Delete

--- controle-emprestimo-chaves/backend/src/main/java/com/GGMN/backend/controller/ChaveController.java
+++ controle-emprestimo-chaves/backend/src/main/java/com/GGMN/backend/controller/ChaveController.java
@@ -2,6 +2,7 @@ package com.GGMN.backend.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,9 +13,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -63,10 +66,47 @@ public class ChaveController {
 			if(chave.getStatus() == true) {
 				chaves.add(chave);
 			}
-		}
-		
-		
+		}	
 		
 		return ResponseEntity.status(HttpStatus.OK).body(chaves);
+	}
+
+	@GetMapping("/nome/{nome}")
+	public ResponseEntity<List<Chave>> getPerNome(@PathVariable(value="nome") String nome) {		
+		
+		List<Chave> chaves = new ArrayList<>();
+		
+		for (Chave chave : chaveRepository.findByNome(nome)) {
+			if(chave.getStatus() == true) {
+				chaves.add(chave);
+			}
+		}	
+		
+		return ResponseEntity.status(HttpStatus.OK).body(chaves);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Chave> updateChave(@PathVariable(value="id") String id, @RequestBody ChaveRecordDto chaveRecordDto) {		
+		
+		Chave chave = new Chave();
+		BeanUtils.copyProperties(chaveRecordDto, chave);
+		
+		chave.setId(id);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(chaveRepository.save(chave));
+		
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Chave> deleteChave(@PathVariable(value="id") String id, @RequestBody ChaveRecordDto chaveRecordDto) {		
+		
+		Chave chave = new Chave();
+		BeanUtils.copyProperties(chaveRecordDto, chave);
+		
+		chave.setStatus(false);
+		chave.setId(id);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(chaveRepository.save(chave));
+		
 	}
 }
--- controle-emprestimo-chaves/backend/src/main/java/com/GGMN/backend/repository/ChaveRepository.java
+++ controle-emprestimo-chaves/backend/src/main/java/com/GGMN/backend/repository/ChaveRepository.java
@@ -1,6 +1,7 @@
 package com.GGMN.backend.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
@@ -11,4 +12,7 @@ import com.GGMN.backend.model.Chave;
 public interface ChaveRepository extends MongoRepository<Chave, String>{
 	List<Chave> findByStatus(boolean status);
 	List<Chave> findBySituacao(String situacao);
+	List<Chave> findByNome(String nome);
+    Object save(Optional<Chave> chave);
+	
 }
